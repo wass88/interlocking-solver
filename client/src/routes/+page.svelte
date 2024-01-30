a852b549a3506638ea1918e0055e64897d6c64cf
wass80 <wasss80@gmail.com>
2024-01-30 22:28:32 +0900

    add  svelte base

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1>Home</h1>
+</section>
+
+<style></style>
